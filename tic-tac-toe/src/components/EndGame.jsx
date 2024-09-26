0b5fee6376b5af57da1a01d07e2cebabe140a51e
PVinaches <palomavinaches@gmail.com>
2024-09-26 13:59:57 +0200

    chore: endGame as modal (added portal)

--- tic-tac-toe/src/components/EndGame.jsx
+++ tic-tac-toe/src/components/EndGame.jsx
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
 import './EndGame.css';
 
 export default function EndGame({ win, restartGame }) {
-  return (
+  return createPortal(
     <div className='end-game'>
       {win == 'draw' && <p>Game finished in draw!</p>}
       {win != 'draw' && <p>Game won by {win}!</p>}
 
       <button onClick={() => restartGame()}>Start again!</button>
-    </div>
+    </div>,
+    document.getElementById('modal')
   );
 }
 
